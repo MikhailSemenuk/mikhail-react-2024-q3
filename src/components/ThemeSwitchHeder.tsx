a209054b40206998b1f0d1d8d9c592859a675714
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-08-09 13:05:30 +0200

    fix: lint-staged error

--- src/components/ThemeSwitchHeder.tsx
+++ src/components/ThemeSwitchHeder.tsx
@@ -1,8 +1,6 @@
-import whereIAm from '@/libs/whereIAm';
 import { ToggleSwitch } from './ToggleSwitch';
 
 export function ThemeSwitchHeder() {
-  whereIAm('ThemeSwitchHeder');
   return (
     <div className='d-flex flex-row-reverse page mt-2'>
       <ToggleSwitch />
--- src/libs/whereIAm.ts
+++
@@ -1,7 +0,0 @@
-export default function whereIAm(name: string) {
-  if (typeof window === 'undefined') {
-    console.log(`Server ☁️: ${name}`);
-  } else {
-    console.log(`Browser 🌐: ${name}`);
-  }
-}
