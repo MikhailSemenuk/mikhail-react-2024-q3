2ec270d3b656eedb537673dfe696a2761bdbe2aa
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-08-17 17:55:10 +0200

    feat: add validation with error in console

--- src/components/UncontrolledForm.tsx
+++ src/components/UncontrolledForm.tsx
@@ -1,14 +1,22 @@
-import { FormEvent } from 'react';
+import { FormEvent, useId } from 'react';
 import { FormItem } from '../types';
 import { useDispatch } from 'react-redux';
 import { addForm } from '../formsSlice';
 import { Link } from 'react-router-dom';
 import { userSchema } from '../validation/UserValidation';
+import { ValidationError } from 'yup';
 
-// TODO: input email already have validation, I guess better disable or rewrite it
+function getNameForm<K extends keyof FormItem>(key: K): K {
+  return key;
+}
 
 export default function UncontrolledForm() {
   const dispatch = useDispatch();
+
+  const nameId = useId();
+  const emailId = useId();
+  const passwordId = useId();
+  const acceptTermsConditionsId = useId();
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -16,16 +24,21 @@ export default function UncontrolledForm() {
     const formData = new FormData(event.currentTarget);
 
     const prepareForm: FormItem = {
-      name: formData.get('name') as string,
-      email: formData.get('email') as string,
-      password: formData.get('password') as string,
-      acceptTermsConditions: formData.get('acceptTermsConditions') === 'on',
+      name: formData.get(getNameForm('name')) as string,
+      email: formData.get(getNameForm('email')) as string,
+      password: formData.get(getNameForm('password')) as string,
+      acceptTermsConditions: formData.get(getNameForm('acceptTermsConditions')) === 'on',
     };
 
-    const isValid = await userSchema.isValid(prepareForm);
-    console.log(`isValid = ${isValid}`);
-    if (isValid) {
+    try {
+      await userSchema.validate(prepareForm, { abortEarly: false });
       dispatch(addForm(prepareForm));
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        err.inner.forEach((error) => {
+          console.log(`Field: ${error.path}, Error: ${error.message}`);
+        });
+      }
     }
   };
 
@@ -34,32 +47,43 @@ export default function UncontrolledForm() {
       <h1>UncontrolledForm page</h1>
       <form onSubmit={handleSubmit}>
         <div className='mb-3'>
-          <label htmlFor='name' className='form-label'>
+          <label htmlFor={nameId} className='form-label'>
             Name
           </label>
-          <input type='text' className='form-control' id='name' name='name' />
+          <input type='text' className='form-control' id={nameId} name={getNameForm('name')} />
         </div>
 
         <div className='mb-3'>
-          <label htmlFor='email' className='form-label'>
+          <label htmlFor={emailId} className='form-label'>
             Email address
           </label>
-          <input type='email' className='form-control' id='email' name='email' aria-describedby='email-help' />
+          <input
+            type='text'
+            className='form-control'
+            id={emailId}
+            name={getNameForm('email')}
+            aria-describedby='email-help'
+          />
           <div id='email-help' className='form-text'>
             We will never share your email with anyone else.
           </div>
         </div>
 
         <div className='mb-3'>
-          <label htmlFor='password' className='form-label'>
+          <label htmlFor={passwordId} className='form-label'>
             Password
           </label>
-          <input type='password' className='form-control' id='password' name='password' />
+          <input type='password' className='form-control' id={passwordId} name={getNameForm('password')} />
         </div>
 
         <div className='mb-3 form-check'>
-          <input type='checkbox' className='form-check-input' id='acceptTermsConditions' name='acceptTermsConditions' />
-          <label className='form-check-label' htmlFor='acceptTermsConditions'>
+          <input
+            type='checkbox'
+            className='form-check-input'
+            id={acceptTermsConditionsId}
+            name={getNameForm('acceptTermsConditions')}
+          />
+          <label className='form-check-label' htmlFor={acceptTermsConditionsId}>
             Check me out
           </label>
         </div>
